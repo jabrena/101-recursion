627f3afc52530b384d41601c52db87ba39b943dc
Juan Antonio Breña Moral <bren@juanantonio.info>
2025-02-05 20:51:19 +0100

    How to show evidences about issues with Stack traces (#24)

--- src/main/java/info/jab/recursion/ShowStackTrace.java
+++ src/main/java/info/jab/recursion/ShowStackTrace.java
@@ -2,18 +2,15 @@ package info.jab.recursion;
 
 public class ShowStackTrace {
     public static void recursiveMethod(int n) {
-        if (n == 0) {
-            printStack();
-            return;
-        }
+        if (n == 0) return;
+        
+        printStack();
         recursiveMethod(n - 1);
     }
 
     public static void printStack() {
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        for (StackTraceElement element : stackTrace) {
-            System.out.println(element);
-        }
+        System.out.println(stackTrace.length);
     }
 }
 
